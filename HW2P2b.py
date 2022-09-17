463960be4586a7b123710db88f6109130681618d
maulikk3886 <mkamdar1@asu.edu>
2022-09-17 01:42:45 -0700

    HW2 problem 2b inital

---
+++ HW2P2b.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def f(x)
+    return (2-(2*x[1])-(3*x[2]))+(x[1]**2)+((x[2]-1)**2)
+def gradient(x)
+    return np.array([(10*x[1+12*x[2]-8]),(12*x[1]+20*x[2]-14)])
+def Hessian(x)
+    return np.array([[10,12],[12,20]])
+
+# Gradient descent algorithm
+t= 0.5
+alpha = 1
+
+if
+    alpha = 0.5 * alpha
+    else
+        break
+
+# Newton's method
